980d641a230100cd4609201eed26fe52b977fb1c
Ahmed <ahmed.shabasy16@gmail.com>
2024-09-09 17:21:16 -0700

    py

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/***
+ * check_cycle - check if cycle
+ * @list: pointer to list
+ * Return: 1 is cycle
+ */
+
+int check_cycle(listint_t *list)
+{
+	listint_t *slow = list, *fast = list;
+
+	while (fast && fast->next)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+
+		if (slow == fast)
+			return (1);
+	}
+	return (0);
